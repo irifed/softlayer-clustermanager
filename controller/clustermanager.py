ddc5f176cb10f87bb11ba7fe7ebc806739095569
Irina Fedulova <fedulova@gmail.com>
2014-11-14 13:45:51 +0300

    retrieve master password

--- controller/clustermanager.py
+++ controller/clustermanager.py
@@ -1,8 +1,11 @@
 import logging
 import uuid
 
+import SoftLayer
+
 from models.models import db, Cluster
-from controller.handle_provisioning import provision_cluster
+from .handle_provisioning import provision_cluster
+
 
 logger = logging.getLogger("endpoint")
 
@@ -16,11 +19,25 @@ def create_cluster(owner_id, sl_config):
     logfile.write('Created cluster {}\n'.format(cluster_id))
 
     # TODO store all cluster parameters, not only num_workers (?)
-    cluster = Cluster(uuid=cluster_id, owner_id=owner_id, num_workers=sl_config.num_workers)
+    cluster = Cluster(
+        uuid=cluster_id,
+        owner_id=owner_id,
+        num_workers=sl_config.num_workers,
+        cpus=sl_config.cpus,
+        memory=sl_config.memory,
+        disk_capacity=sl_config.disk_capacity,
+        network_speed=sl_config.network_speed,
+
+        sl_username=sl_config.sl_username,
+        sl_api_key=sl_config.sl_api_key,
+        # TODO sl_ssh_keys
+        sl_domain=sl_config.sl_domain,
+        sl_datacenter=sl_config.sl_datacenter
+    )
     db.session.add(cluster)
     db.session.commit()
 
-    ## DEBUG
+    # # DEBUG
     # cmd = 'sl vs create -y --hourly --datacenter=dal06 --cpu=4 --memory=16384 --os=UBUNTU_LATEST --domain=bdasmarket.irina.com --hostname=controller --wait=86400 --key=irina@ru.ibm.com'
     # p = Popen(cmd, shell=True, bufsize=10000, stdin=PIPE, stdout=logfile, stderr=STDOUT)
     ## END DEBUG
@@ -43,5 +60,17 @@ def destroy_cluster(cluster_id):
     # TODO call vagrant destroy for this cluster
 
 
-def get_master_password(master_ip, owner_id):
-    pass
+def get_master_password(master_ip, cluster_id):
+    # retrieve sl username and api key by cluster_id
+    cluster = Cluster.by_uuid(cluster_id)
+
+    client = SoftLayer.Client(username=cluster.sl_username,
+                              api_key=cluster.sl_api_key)
+
+    vs_manager = SoftLayer.managers.VSManager(client)
+    master_details = vs_manager.list_instances(public_ip=master_ip)
+    master_instance = vs_manager.get_instance(instance_id=master_details[0]['id'])
+    master_password = master_instance['operatingSystem']['passwords']
+    return master_password
+
+
