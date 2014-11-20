0a255c59d38daa4204caa178b6da24d37a53db26
Alan <anewman@us.ibm.com>
2014-11-20 14:26:42 -0800

    -I'm really bad at this, and don't want to lose changes : / --I know I'm pushing to the master branch though : / --got weird errors when I tried to push to the new one (attempting to save stash first) : (

--- endpoint/endpoint.py
+++ endpoint/endpoint.py
@@ -10,15 +10,16 @@ import logging
 from xml.dom import minidom
 
 import oauth2
-from flask import request, Response, render_template, redirect
-
-from endpoint import app
+from flask import request, Response, render_template, redirect, flash,session
+import traceback
+from . import app
+import sys
 
 from models.models import Cluster
 from models.sl_config import SLConfig
 from controller.clustermanager import create_cluster, get_master_password
 from controller.handle_provisioning import get_cluster_status
-
+import SoftLayer
 from .marshall import EventXml
 from .events import HandleEvent
 
@@ -32,6 +33,95 @@ event_url_template = 'https://www.appdirect.com/api/integration/v1/events/{}'
 # AppDirect credentials
 consumer_key = 'bdas-cluster-15877'
 consumer_secret = 'PMBysgKaJtWZrOiq'
+
+def getClient(username,apikey):
+    # Note: this does not raise a SL exception, even if the api key is wrong
+    return SoftLayer.Client(username=username, api_key=apikey, endpoint_url=SoftLayer.API_PUBLIC_ENDPOINT)
+# def verifyCredentials():
+#     '''Make a trivial api call to SL to verify the username and api key.'''
+#     try:
+#         user = self.client['Account'].getCurrentUser(mask='email')
+#         return 200, 'SoftLayer credentials ok'
+#     except SoftLayer.exceptions.SoftLayerAPIError as e:
+#         if isAuthProb(e.faultString):  return 403, str(e.faultCode) + ': ' + e.faultString
+#         else:  return 502, str(e.faultCode) + ': ' + e.faultString
+
+@app.route("/")
+def index():
+
+    return render_template('uilogin.html')
+
+@app.route('/uilogin', methods=['GET', 'POST'])
+def login():
+    try:
+        if "username" in request.form:
+            userName = request.form['username']
+        else:
+            userName = None
+        if "apikey" in request.form:
+            apiKey = request.form['apikey']
+        else:
+            apiKey = None
+
+        if (request.method == 'POST') and userName!=None and apiKey!=None:
+    
+            # User id and API key have been submitted so now we need to authenticate the user
+            # and check for any missing ACLs that might later halt the process.
+            #
+
+            try: 
+
+                client = getClient(userName,apiKey)
+                user = client['Account'].getCurrentUser(mask='email')
+                useremail = user["email"]
+
+            except SoftLayer.exceptions.SoftLayerAPIError as e:
+                flash("Failed Authentication")
+                return redirect("/")
+
+            except Exception as e:
+                msg = ["Internal error occurred:"+e.args[0]+". Please contact support."]
+                flash(msg)
+                traceback.print_exc()
+                return redirect("/")
+
+            # If the user was authenticated then we can store the user's info in the HTTP session
+            # object and then begin the workflow for gathering config info
+            #
+            session['username'] = userName
+            session['useremail'] = useremail
+            session['apikey'] = apiKey
+
+            return _create_cluster()
+
+        elif request.method =="POST":
+            # The user did not specify all the required login information.
+            #
+            if ( not userName ):
+                msg = ['Please specify your SoftLayer user id.']
+            elif (not apiKey):
+                msg = ['Please specify your SoftLayer API Key.']
+            else:
+                msg = ['Please specify your email address when acting as an agent for the user.']
+            flash(msg)
+            return redirect("/")
+
+        else:
+            return redirect("/")
+
+    except Exception as e:
+        traceback.print_exc()
+        msg = ["Internal error occurred:"+e.args[0]+". Please contact support."]
+        flash(msg)
+
+    return redirect("/uilogin")
+
+@app.route('/uilogout')
+def logout():
+    session.pop('username', None)
+    session.pop('apikey', None)
+    session.pop("useremail")
+    return redirect('/')
 
 @app.route('/login', methods=['POST', 'GET'])
 def _login():
@@ -84,12 +174,12 @@ def _create_cluster():
 
     form = SLConfigForm()
     if form.validate_on_submit():
-        logging.debug('sl_username={}, sl_api_key={}, sl_ssh_key={}, '
+        logging.debug('sl_ssh_key={}, '
                       'sl_domain={},'
                       'sl_datacenter={}, num_workers={}, '
                       'sl_cpus={}, sl_memory={},'
                       'sl_disk_capacity={}, sl_network_speed={}'.format(
-            form.sl_username.data, form.sl_api_key.data, form.sl_ssh_key.data,
+            form.sl_ssh_key.data,
             form.sl_domain.data,
             form.sl_datacenter.data, form.num_workers.data,
             form.sl_cpus.data, form.sl_memory.data,
@@ -103,8 +193,8 @@ def _create_cluster():
             ssh_keys.append(form.sl_ssh_key.data)
 
         sl_config = SLConfig(
-            sl_username=form.sl_username.data,
-            sl_api_key=form.sl_api_key.data,
+            sl_username=session["username"],
+            sl_api_key=session["apikey"],
 
             sl_ssh_keys=ssh_keys,
             sl_private_key_path=private_key_path,
@@ -119,7 +209,7 @@ def _create_cluster():
 
         return redirect('/cluster_status?cluster_id={}'.format(cluster_id))
 
-    return render_template('form.html', title='Home page', form=form)
+    return render_template('form.html', title='Home page', form=form,username=session["username"])
 
 
 @app.route('/master_ip', methods=['POST', 'GET'])
