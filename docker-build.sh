89c7326d82aa608ebf01ced3806f50241ba33211
Irina Fedulova <fedulova@gmail.com>
2014-11-23 23:47:03 +0300

    added comment for docker-build

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Build Docker image based on Phusion and including Python Flask, Vagrant and Ansible.
+# This image is publicly available in Dockerhub,
+# so there is no need to run docker-build.sh every time.
+# docker-build is necessary only if additional dependency is required in image,
+# e.g. additional python package.
+
 sudo docker build -t "irifed/flask_vagrant_ansible" .
 
